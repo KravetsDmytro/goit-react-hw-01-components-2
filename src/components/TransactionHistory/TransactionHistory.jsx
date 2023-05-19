bb040aa7c9cb0aded88c0afbfe7c38fd1972c0c1
Kravets_Dmytro <ditur@ukr.net>
2023-05-19 16:49:22 +0300

    transaction =css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -5,10 +5,10 @@ function TransactionHistory ({items}){
 
 <table className={css['transaction-history']}>
   <thead  className={css['transaction-head']} >
-    <tr className=''>
-      <th className=''>Type</th>
-      <th className=''>Amount</th>
-      <th className=''>Currency</th>
+    <tr className={css['transaction-title']}>
+      <th className={css['transaction-title']}>Type</th>
+      <th className={css['transaction-title']}>Amount</th>
+      <th className={css['transaction-title']}>Currency</th>
     </tr >
   </thead >
 {items.map(item=>
