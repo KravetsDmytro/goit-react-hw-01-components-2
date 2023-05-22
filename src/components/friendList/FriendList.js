102d871a35993711f30f826d008adc7e39471ce2
Kravets_Dmytro <ditur@ukr.net>
2023-05-22 13:58:18 +0300

    Friendlist +css

--- src/components/friendList/FriendList.js
+++ src/components/friendList/FriendList.js
@@ -1,7 +1,11 @@
+import PropTypes from 'prop-types';
 import FriendListItem from "./friendListItem";
+import css from './friendList.module.css';
+
 
 function FriendList({friends}){
-return ( <ul className="css">
+return (
+   <ul className={css['friend-list']}>
 {friends.map(({ id, avatar, name, isOnline }) => (
   <FriendListItem
     key={id}
@@ -14,12 +18,17 @@ return ( <ul className="css">
 </ul>
 
 
-
 )
 
-
-
 }
+
+FriendListItem.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 
 export default FriendList
